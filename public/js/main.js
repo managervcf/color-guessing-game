db0953d3c37309bb302624b1e1149782ef4825b8
Mateusz Pyzowski <managervcf@gmail.com>
2018-07-26 10:19:26 +0000

    Change event listener to reset() only on 'space' & 'enter'

--- public/js/main.js
+++ public/js/main.js
@@ -105,8 +105,10 @@ function randomColor() {
 }
 
 
-wholeBody.addEventListener("keydown", function() {
-	reset();
+wholeBody.addEventListener("keydown", function(event) {
+	if (event.keyCode === 13 || event.keyCode === 32) {
+		reset();	
+	}
 });
 
 newGameButton.addEventListener("click", function() {
