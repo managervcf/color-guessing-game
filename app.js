25c78f39132217948a1514a69f070c1dc27ea629
Mateusz Pyzowski <managervcf@gmail.com>
2018-07-26 08:15:35 +0000

    Require 'public/' directory

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const   express = require("express"),
         app     = express();
-    
+
+
+app.use(express.static(__dirname + "/public"));    
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
