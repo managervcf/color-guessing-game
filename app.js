b19f4952c35404f02eb971a81aac433d5ac100e2
Mateusz Pyzowski <managervcf@gmail.com>
2018-11-10 20:13:33 +0800

    Add local port 3000 and log message on server start

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const   express = require("express"),
-        app     = express();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));    
 app.set("view engine", "ejs");
@@ -8,4 +9,5 @@ app.get("/", (req, res) => res.render("game"));
 
 app.get("/*", (req, res) => res.redirect("/"));
 
-app.listen(process.env.PORT, process.env.IP);
+app.listen(port, () =>
+	console.log('Server is running on port ' + port));
