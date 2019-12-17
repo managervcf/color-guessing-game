a95db92f61027719c8c6d584552680fb61169868
Mateusz <managervcf@gmail.com>
2019-12-17 08:51:51 +0100

    Import path module to use resolve method. Add extra comments

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // Serve static files
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+// Send index.html on any route
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'index.html')));
 
+// Start the server
 app.listen(process.env.PORT || 3000, () => console.log('Server running...'));
